2ff50353eda022a9164153d5bd87e5168b050e15
chan <cboswell1024@gmail.com>
2021-08-14 13:10:33 -0400

    initial playground

---
+++ index.js
@@ -0,0 +1,17 @@
+function stepOne () {
+    document.getElementById("got-it-sec-2-1").style.backgroundColor="lavender";
+    document.getElementById("got-it-sec-2-1").style.border="1px solid lavender";
+    document.getElementById("step-one").style.color="lavender";
+    document.getElementById("setting-pointer").style.color="black";
+    document.getElementById("rr-1").style.color="black";
+    document.getElementById("rr-2").style.color="black";
+    document.getElementById('iteration-1').style.backgroundColor="black";
+    document.getElementById('iteration-1').style.border="1px solid black";
+    document.getElementById("pointer-11").style.color="black";
+    document.getElementById("pointer-22").style.color="black";
+    document.getElementById("got-it-sec-2-2").style.color="lavender";
+    document.getElementById("got-it-sec-2-2").style.border="1px solid black";
+
+
+
+}
