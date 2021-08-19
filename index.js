48ec9c8cbfb43dabb12d3b0c7bd5ef42490b8355
chan <cboswell1024@gmail.com>
2021-08-19 11:28:40 -0400

    finished visualiation

--- index.js
+++ index.js
@@ -15,3 +15,71 @@ function stepOne () {
 
 
 }
+
+function stepTwo() {
+    document.getElementById("increment-a").style.color="pink";
+    document.getElementById("got-it-a").style.border="1px solid pink";
+    document.getElementById("got-it-a").style.backgroundColor="pink";
+    document.getElementById("got-it-a").style.color="pink";
+    document.getElementById('a-move-2').style.backgroundColor='black';
+    document.getElementById('a-move-1').style.backgroundColor='black';
+    document.getElementById('compare-a').style.color='black';
+    document.getElementById('equal-a').style.color='black';
+    document.getElementById('next-iteration-a').style.color='pink';
+    document.getElementById('next-iteration-a').style.backgroundColor='black';
+
+    document.getElementById('next-iteration-a').style.border="1px solid black";
+    document.getElementById('pointer-1-3').style.color='black';
+    document.getElementById('pointer-2-3').style.color='black';
+
+
+
+
+
+}
+
+
+function stepThree() {
+    document.getElementById('increment-c').style.color='lavender';
+    document.getElementById('got-it-c').style.border="1px solid lavender";
+    document.getElementById('got-it-c').style.color='lavender';
+    document.getElementById('got-it-c').style.backgroundColor='lavender';
+    document.getElementById('pointer-c').style.color='black';
+    document.getElementById('pointer-c-2').style.color='black';
+    document.getElementById('right-c').style.backgroundColor='black';
+    document.getElementById('left-c').style.backgroundColor='black';
+    document.getElementById('compare-c').style.color='black';
+    document.getElementById('equal-c').style.color='black';
+    document.getElementById('next-iteration-c').style.backgroundColor='black';
+    document.getElementById('next-iteration-c').style.border="1px solid black";
+
+
+}
+
+
+function stepFour() {
+    document.getElementById('tricky-c').style.color='pink';
+    document.getElementById('got-it-e').style.backgroundColor='pink';
+    document.getElementById('got-it-e').style.border=' 1px solid pink';
+    document.getElementById('pointer-e').style.color='black';
+    document.getElementById('pointer-e-2').style.color='black';
+    document.getElementById('loop-e').style.backgroundColor='black';
+    document.getElementById('right-e').style.backgroundColor='black';
+    document.getElementById('left-e').style.backgroundColor='black';
+    document.getElementById('compare-e').style.color='black';
+    document.getElementById('equal-e').style.color='black';
+    document.getElementById('palindrome-e').style.color='black';
+    document.getElementById('statement-e').style.color='black';
+    document.getElementById('smiley-e').style.backgroundColor='black';
+    document.getElementById('smiley-e').style.border='1px solid black';
+    
+    
+    
+    
+
+
+
+
+
+
+}
